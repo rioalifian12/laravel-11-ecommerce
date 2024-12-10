a4f9e969a0465c901a26c94a6e1c9af6171c80b5
rioalifian12 <rioalifian12@gmail.com>
2024-12-11 02:24:05 +0700

    refactor function add to cart

--- resources/views/show_product.blade.php
+++ resources/views/show_product.blade.php
@@ -14,13 +14,21 @@
     <p>Stock: {{ $product->stock }}</p>
     <img src="{{ url('storage/' . $product->image) }}" alt="{{ $product->image }}" height="100px">
     <br>
+    
     <form action="{{ route('edit_product', $product) }}" method="get">
         <button type="submit">Edit</button>
     </form>
+
     <form action="{{ route('add_to_cart', $product) }}" method="post">
         @csrf
         <input type="number" name="amount" value=1>
         <button type="submit">Add to cart</button>
     </form>
+    {{-- menampilkan error --}}
+    @if ($errors->any())
+        @foreach ($errors->all() as $error)
+            <p>{{ $error }}</p>
+        @endforeach
+    @endif
 </body>
 </html>
