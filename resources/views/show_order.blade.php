fd5c28113cac0ede1e2652cb76b4ed674eea5535
rioalifian12 <rioalifian12@gmail.com>
2024-12-11 04:14:17 +0700

    add function submit payment receipt, update view show order, add routing submit payment receipt

--- resources/views/show_order.blade.php
+++ resources/views/show_order.blade.php
@@ -13,5 +13,16 @@
         <p>Product: {{ $transaction->product->name }}</p>
         <p>Amount: {{ $transaction->amount }}</p>
     @endforeach
+
+    @if ($order->is_paid == false && $order->payment_receipt == null)
+        <form action="{{ route('submit_payment_receipt', $order) }}" method="post" enctype="multipart/form-data">
+            @csrf
+            <label for="payment_receipt">Upload your payment receipt</label><br>
+            <input type="file" name="payment_receipt" id="payment_receipt"><br>
+            <button type="submit">Submit payment</button>
+        </form>
+    @else
+        
+    @endif
 </body>
 </html>
